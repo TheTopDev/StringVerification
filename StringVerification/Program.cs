2ba933ce2b025c2fe6be6bed10520326622f8c88
Lucas Garofalo <garofalodev@gmail.com>
2021-03-07 18:46:38 -0600

    This program takes in two inputs, then checks to see if they match

---
+++ StringVerification/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString, myRegularExpression;
+            bool matchState = true;
+
+            do 
+            {
+                Console.WriteLine("The default regular expression checks for at least one digit. ");
+                Console.Write("Enter a refular expression (or press ENTER to use the default): ");
+                myRegularExpression = Console.ReadLine();
+                var strChecker = new Regex(@myRegularExpression);
+                Console.Write("Enter some input: ");
+                myString = Console.ReadLine();
+                matchState = strChecker.IsMatch(myString);
+                Console.WriteLine("{0} matches {1}? {2}", myString, myRegularExpression, matchState.ToString());
+                Console.WriteLine("Press ESC to terminate program or press any other key to try again. ");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
